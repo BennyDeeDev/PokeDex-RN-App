9f337b1d72101f21c7b2b486bd7110d4bbc4bae6
Benjamin Derksen <45900418+derkseb@users.noreply.github.com>
2022-05-23 17:47:51 +0200

    pokemon sprites and text on app

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useCallback, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 
 const pokePath = "https://pokeapi.co/api/v2/";
-const pokeQuery = "pokemon?limit=3&offset=0";
+const pokeQuery = "pokemon?limit=151&offset=0";
 const firstGenPokemonPath = `${pokePath}${pokeQuery}`;
 
 //1 Call Pokemon IDs --> 151 = 152 Calls to the API
@@ -19,19 +19,39 @@ export default function App() {
       const firstGenPokemonDetails = await Promise.all(
         firstGenPokemonIdsBody.results.map(async (p) => {
           const pDetails = await fetch(p.url);
+
           return await pDetails.json();
         })
       );
-      console.log(firstGenPokemonDetails);
+
       setfirstGenPokemonDetails(firstGenPokemonDetails);
     };
 
     fetchFirstGenPokemons();
   }, []);
 
+  const renderPokemon = ({ item }) => {
+    return (
+      <View style={{ backgroundColor: "lightgrey", marginTop: 10 }}>
+        <Text style={{ fontSize: 32, alignSelf: "center", marginTop: 10 }}>
+          {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+        </Text>
+        <Image
+          style={{ width: 200, height: 200, alignSelf: "center" }}
+          source={{
+            uri: item.sprites.front_default,
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{JSON.stringify(firstGenPokemonDetails)}</Text>
+      <Text style={{ fontSize: 38, alignSelf: "center", marginBottom: 20 }}>
+        First Gen Pokemons
+      </Text>
+      <FlatList data={firstGenPokemonDetails} renderItem={renderPokemon} />
 
       <StatusBar style="auto" />
     </View>
@@ -42,7 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: 60,
   },
+  pokemonContainer: {},
 });
