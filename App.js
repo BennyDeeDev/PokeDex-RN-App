de7dac77712be35c559231f53abfb39b7db7d84e
Benjamin Derksen <45900418+derkseb@users.noreply.github.com>
2022-05-23 17:20:37 +0200

    init + timeout module

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useCallback, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const pokePath = "https://pokeapi.co/api/v2/";
+const pokeQuery = "pokemon?limit=3&offset=0";
+const firstGenPokemonPath = `${pokePath}${pokeQuery}`;
+
+//1 Call Pokemon IDs --> 151 = 152 Calls to the API
 
 export default function App() {
+  const [firstGenPokemonDetails, setfirstGenPokemonDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchFirstGenPokemons = async () => {
+      const firstGenPokemonIdsResponse = await fetch(firstGenPokemonPath);
+      const firstGenPokemonIdsBody = await firstGenPokemonIdsResponse.json();
+
+      const firstGenPokemonDetails = await Promise.all(
+        firstGenPokemonIdsBody.results.map(async (p) => {
+          const pDetails = await fetch(p.url);
+          return await pDetails.json();
+        })
+      );
+      console.log(firstGenPokemonDetails);
+      setfirstGenPokemonDetails(firstGenPokemonDetails);
+    };
+
+    fetchFirstGenPokemons();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{JSON.stringify(firstGenPokemonDetails)}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
